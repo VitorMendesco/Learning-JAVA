42272a168e840b5badcd0fb8f2f5ad7c30f3a370
VitorMendesco <vitor.mcosta@sptech.school>
2022-07-01 11:36:21 -0300

    print with '\n' and concatenation

--- src/com/mendesco/HelloWorld.java
+++ src/com/mendesco/HelloWorld.java
@@ -6,17 +6,34 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		// data and variables
 		// integer numbers
-		byte verySmallRange; // -128 -> 127
-		short smallRange; // -32,768 -> 32,767
-		int mediumRange; // -2,147,483,648 -> 2,147,483,647
-		long longRange; // -9,223,372,036,854,775,808 -> 9,223,372,036,854,775,807
+		byte verySmallRange = 127; // -128 -> 127
+		short smallRange = 1000; // -32,768 -> 32,767
+		int mediumRange = 1200300; // -2,147,483,648 -> 2,147,483,647
+		long longRange = 300000000; // -9,223,372,036,854,775,808 -> 9,223,372,036,854,775,807
 		// decimal numbers
-		float lessAccurate; // 3.40282347 x 1038 -> 1.40239846 x 10-45
-		double moreAccurate; // 1.7976931348623157 x 10308 -> 4.9406564584124654 x 10-324 | 2x more than FLOAT
+		float lessAccurate = 14.8f; // 3.40282347 x 1038 -> 1.40239846 x 10-45
+		double moreAccurate = 14.8290; // 1.7976931348623157 x 10308 -> 4.9406564584124654 x 10-324 | 2x more than FLOAT
 		// strings
-		char oneLetter;
-		String moreLetters;
+		char oneLetter = 'V';
+		String moreLetters = "Vitor";
 		// boolean
-		boolean trueOrFalse;
+		boolean trueOrFalse = true;
+		
+		System.out.println(
+				"---NUMBERS---\n" + 
+				"byte: " + verySmallRange + " (very small range)\n" + 
+				"short: " + smallRange + " (small range)\n" + 
+				"int: " + mediumRange + " (medium range)\n" + 
+				"long: " + longRange + " (long range)\n" +
+				"float: " + lessAccurate + " (decimal - less accuracy)\n" + 
+				"double: " + moreAccurate + " (decimal - more accuracy)\n" + 
+				"\n" + 
+				"---STRINGS---\n" + 
+				"char: " + oneLetter + " (just one letter)\n" + 
+				"String: " + moreLetters + " (more letters)\n" + 
+				"\n" +
+				"---BOOL---\n" + 
+				"boolean: " + trueOrFalse + " (boolean: true ? false)\n"
+				);
 	}
 }
