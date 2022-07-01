9a59527059618a0eec16c8eee62d031c316e1bed
VitorMendesco <vitor.mcosta@sptech.school>
2022-07-01 12:03:23 -0300

    conversions

--- src/com/mendesco/HelloWorld.java
+++ src/com/mendesco/HelloWorld.java
@@ -4,36 +4,56 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// print in output
 		System.out.println("Hello World!");
-		// data and variables
+		
+		/*-------------------DATA AND VARIABLES-------------------*/
 		// integer numbers
-		byte verySmallRange = 127; // -128 -> 127
-		short smallRange = 1000; // -32,768 -> 32,767
-		int mediumRange = 1200300; // -2,147,483,648 -> 2,147,483,647
-		long longRange = 300000000; // -9,223,372,036,854,775,808 -> 9,223,372,036,854,775,807
+		byte byteVar = 127; // -128 -> 127
+		short shortVar = 1000; // -32,768 -> 32,767
+		int intVar = 1200300; // -2,147,483,648 -> 2,147,483,647
+		long longVar = 300000000; // -9,223,372,036,854,775,808 -> 9,223,372,036,854,775,807
 		// decimal numbers
-		float lessAccurate = 14.8f; // 3.40282347 x 1038 -> 1.40239846 x 10-45
-		double moreAccurate = 14.8290; // 1.7976931348623157 x 10308 -> 4.9406564584124654 x 10-324 | 2x more than FLOAT
+		float floatVar = 14.8f; // 3.40282347 x 1038 -> 1.40239846 x 10-45
+		double doubleVar = 14.8290; // 1.7976931348623157 x 10308 -> 4.9406564584124654 x 10-324 | 2x more than FLOAT
 		// strings
-		char oneLetter = 'V';
-		String moreLetters = "Vitor";
+		char charVar = '0';
+		String stringVar = "Vitor";
 		// boolean
-		boolean trueOrFalse = true;
+		boolean booleanVar = true;
 		
 		System.out.println(
-				"---NUMBERS---\n" + 
-				"byte: " + verySmallRange + " (very small range)\n" + 
-				"short: " + smallRange + " (small range)\n" + 
-				"int: " + mediumRange + " (medium range)\n" + 
-				"long: " + longRange + " (long range)\n" +
-				"float: " + lessAccurate + " (decimal - less accuracy)\n" + 
-				"double: " + moreAccurate + " (decimal - more accuracy)\n" + 
-				"\n" + 
+				"\n---NUMBERS---\n" + 
+				"byte: " + byteVar + " (very small range)\n" + 
+				"short: " + shortVar + " (small range)\n" + 
+				"int: " + intVar + " (medium range)\n" + 
+				"long: " + longVar + " (long range)\n" +
+				"float: " + floatVar + " (decimal - less accuracy)\n" + 
+				"double: " + doubleVar + " (decimal - more accuracy)\n" + 
 				"---STRINGS---\n" + 
-				"char: " + oneLetter + " (just one letter)\n" + 
-				"String: " + moreLetters + " (more letters)\n" + 
-				"\n" +
+				"char: " + charVar + " (just one letter)\n" + 
+				"String: " + stringVar + " (more letters)\n" + 
 				"---BOOL---\n" + 
-				"boolean: " + trueOrFalse + " (boolean: true ? false)\n"
+				"boolean: " + booleanVar + " (boolean: true ? false)\n"
+				);
+		
+		/*-------------------CONVERSIONS-------------------*/
+		// possible conversions:		
+			short byteShort = (short) byteVar; // byte -> short;			
+			int shortInt = (int) shortVar; // short -> int;			
+			int charInt = (int) charVar; // char -> int;			
+			long intLong = (long) intVar; // int -> long;			
+			double intDouble = (double) intVar; // int -> double;
+			float intFloat = (float) intVar; // int -> float; (information loss)		
+			float longFloat = (float) longVar; // long -> float; (information loss)
+			
+		System.out.println(
+				"\n---CONVERSIONS---\n" + 
+				"byte -> short: " + byteShort + "\n" +
+				"short -> int: " + shortInt + "\n" +		
+				"char-> int: " + charInt + "\n" +		
+				"int -> long: " + intLong + "\n" +		
+				"int -> double: " + intDouble + "\n" +		
+				"int -> float: " + intFloat + "\n" +		
+				"long -> float: " + longFloat + "\n"		
 				);
 	}
 }
