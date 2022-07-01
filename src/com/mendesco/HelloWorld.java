5a77332dffc503d18bc2c0d9ec21e4fa8715478e
VitorMendesco <vitor.mcosta@sptech.school>
2022-07-01 12:43:26 -0300

    reading, operations and conditionals

--- src/com/mendesco/HelloWorld.java
+++ src/com/mendesco/HelloWorld.java
@@ -1,10 +1,12 @@
 package com.mendesco;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		// print in output
 		System.out.println("Hello World!");
-		
+
 		/*-------------------DATA AND VARIABLES-------------------*/
 		// integer numbers
 		byte byteVar = 127; // -128 -> 127
@@ -19,41 +21,89 @@ public class HelloWorld {
 		String stringVar = "Vitor";
 		// boolean
 		boolean booleanVar = true;
-		
+
 		System.out.println(
-				"\n---NUMBERS---\n" + 
-				"byte: " + byteVar + " (very small range)\n" + 
-				"short: " + shortVar + " (small range)\n" + 
-				"int: " + intVar + " (medium range)\n" + 
-				"long: " + longVar + " (long range)\n" +
-				"float: " + floatVar + " (decimal - less accuracy)\n" + 
-				"double: " + doubleVar + " (decimal - more accuracy)\n" + 
-				"---STRINGS---\n" + 
-				"char: " + charVar + " (just one letter)\n" + 
-				"String: " + stringVar + " (more letters)\n" + 
-				"---BOOL---\n" + 
-				"boolean: " + booleanVar + " (boolean: true ? false)\n"
+				"---NUMBERS---\n" + 
+						"byte: " + byteVar + " (very small range)\n" + 
+						"short: " + shortVar + " (small range)\n" + 
+						"int: " + intVar + " (medium range)\n" + 
+						"long: " + longVar + " (long range)\n" +
+						"float: " + floatVar + " (decimal - less accuracy)\n" + 
+						"double: " + doubleVar + " (decimal - more accuracy)\n" + 
+						"---STRINGS---\n" + 
+						"char: " + charVar + " (just one letter)\n" + 
+						"String: " + stringVar + " (more letters)\n" + 
+						"---BOOL---\n" + 
+						"boolean: " + booleanVar + " (boolean: true ? false)\n"
 				);
-		
+
 		/*-------------------CONVERSIONS-------------------*/
 		// possible conversions:		
-			short byteShort = (short) byteVar; // byte -> short;			
-			int shortInt = (int) shortVar; // short -> int;			
-			int charInt = (int) charVar; // char -> int;			
-			long intLong = (long) intVar; // int -> long;			
-			double intDouble = (double) intVar; // int -> double;
-			float intFloat = (float) intVar; // int -> float; (information loss)		
-			float longFloat = (float) longVar; // long -> float; (information loss)
-			
+		short byteShort = (short) byteVar; // byte -> short;			
+		int shortInt = (int) shortVar; // short -> int;			
+		int charInt = (int) charVar; // char -> int;			
+		long intLong = (long) intVar; // int -> long;			
+		double intDouble = (double) intVar; // int -> double;
+		float intFloat = (float) intVar; // int -> float; (information loss)		
+		float longFloat = (float) longVar; // long -> float; (information loss)
+
 		System.out.println(
 				"\n---CONVERSIONS---\n" + 
-				"byte -> short: " + byteShort + "\n" +
-				"short -> int: " + shortInt + "\n" +		
-				"char-> int: " + charInt + "\n" +		
-				"int -> long: " + intLong + "\n" +		
-				"int -> double: " + intDouble + "\n" +		
-				"int -> float: " + intFloat + "\n" +		
-				"long -> float: " + longFloat + "\n"		
+						"byte -> short: " + byteShort + "\n" +
+						"short -> int: " + shortInt + "\n" +		
+						"char-> int: " + charInt + "\n" +		
+						"int -> long: " + intLong + "\n" +		
+						"int -> double: " + intDouble + "\n" +		
+						"int -> float: " + intFloat + "\n" +		
+						"long -> float: " + longFloat + "\n"		
 				);
+
+		/*-------------------READING, OPERATIONS AND CONDITIONALS-------------------*/
+		Scanner scannerVar = new Scanner(System.in);
+
+		System.out.println("\n---READING, OPERATIONS AND CONDITIONALS---\n");
+
+		System.out.print("Type one number: "); // println jumps one line after log
+		float num1Var = scannerVar.nextFloat(); // nextSomething read some type of variable
+		System.out.print("Type a second number: ");
+		float num2Var = scannerVar.nextFloat();
+
+		System.out.print(
+				"\nNow choose an operation: \n" + 
+						"+ : 1\n" + 
+						"- : 2\n" + 
+						"x : 3\n" + 
+						"/ : 4\n"
+				);
+		byte operationVar = scannerVar.nextByte();
+
+		float resultVar = num1Var;
+		String finalResult = "Result: ";
+		switch (operationVar) {
+		case 1:
+			resultVar += num2Var;
+			finalResult += String.valueOf(resultVar);
+			break;
+
+		case 2: 
+			resultVar -= num2Var;
+			finalResult += String.valueOf(resultVar);
+			break;
+
+		case 3: 
+			resultVar *= num2Var;
+			finalResult += String.valueOf(resultVar);
+			break;
+
+		case 4: 
+			resultVar /= num2Var;
+			finalResult += String.valueOf(resultVar);
+			break;
+
+		default:
+			finalResult = "Type 1, 2, 3 or 4";
+		}
+
+		System.out.println(finalResult);
 	}
 }
