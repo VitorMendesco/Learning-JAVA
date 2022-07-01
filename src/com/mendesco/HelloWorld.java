ff64a974f5f417ea98ef4ae5ef6c4a030c5578ad
VitorMendesco <vitor.mcosta@sptech.school>
2022-07-01 11:20:33 -0300

    decimal numbers, strings and boolean type

--- src/com/mendesco/HelloWorld.java
+++ src/com/mendesco/HelloWorld.java
@@ -11,10 +11,12 @@ public class HelloWorld {
 		int mediumRange; // -2,147,483,648 -> 2,147,483,647
 		long longRange; // -9,223,372,036,854,775,808 -> 9,223,372,036,854,775,807
 		// decimal numbers
-		float lessPrecise; // 3.40282347 x 1038 -> 1.40239846 x 10-45
-		double morePrecise; // 1.7976931348623157 x 10308 -> 4.9406564584124654 x 10-324
-		/*--------------------*/
-		// STRINGS
-		
+		float lessAccurate; // 3.40282347 x 1038 -> 1.40239846 x 10-45
+		double moreAccurate; // 1.7976931348623157 x 10308 -> 4.9406564584124654 x 10-324 | 2x more than FLOAT
+		// strings
+		char oneLetter;
+		String moreLetters;
+		// boolean
+		boolean trueOrFalse;
 	}
 }
